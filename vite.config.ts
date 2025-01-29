6a6070b1d427de7a913596e8d217caae467722af
Lunar-Dawn <lunarrequiem42@gmail.com>
2025-01-30 00:44:14 +0100

    Prep for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({command}) => {
+	return {
+		plugins: [vue()],
+		base: command == 'build' ? '/mesbg_roll_helper/' : '/',
+	}
 })
